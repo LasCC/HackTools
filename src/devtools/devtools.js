0f5a2da95b70af9b59f4c4abf34cfa4a169f61be
LasCC <erazjean@gmail.com>
2020-06-23 00:33:31 +0200

    Update TTY Spawn Shell 🍦

--- src/devtools/devtools.js
+++ src/devtools/devtools.js
@@ -1,5 +1,5 @@
 chrome.devtools.panels.create(
-  "BOPPP", // title for the panel tab
+  "HackTools", // title for the panel tab
   null, // you can specify here path to an icon
   "index.html", // html page which is gonna be injecting into the tab's content
   null // you can pass here a callback function
