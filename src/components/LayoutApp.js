d48b7312499e58de89d1b1e7d9516b501e679b8a
LasCC <erazjean@gmail.com>
2020-06-23 02:01:32 +0200

    Update all svg icons and update layout ❇️

--- src/components/LayoutApp.js
+++ src/components/LayoutApp.js
@@ -1,12 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import { Layout, Menu } from "antd";
-import {
-  DesktopOutlined,
-  PieChartOutlined,
-  FileOutlined,
-  TeamOutlined,
-  CopyrightCircleOutlined,
-} from "@ant-design/icons";
+import { CopyrightCircleOutlined } from "@ant-design/icons";
+import { createFromIconfontCN } from "@ant-design/icons";
 import { goTo } from "react-chrome-extension-router";
 import ReverseShell from "./ReverseShell";
 import PhpReverseShell from "./PhpReverseShell";
@@ -14,6 +9,9 @@ import TtySpawnShell from "./TtySpawnShell";
 
 const { Sider, Content, Footer } = Layout;
 const { SubMenu } = Menu;
+const IconFont = createFromIconfontCN({
+  scriptUrl: ["./iconfont.js"],
+});
 
 export default (props) => {
   return (
@@ -23,29 +21,40 @@ export default (props) => {
         <Menu theme='dark' defaultSelectedKeys={["1"]} mode='inline'>
           <Menu.Item
             key='1'
-            icon={<PieChartOutlined />}
+            icon={<IconFont type='icon-gnubash' />}
             onClick={() => goTo(ReverseShell)}
           >
             Reverse Shell
           </Menu.Item>
           <Menu.Item
             key='2'
-            icon={<DesktopOutlined />}
+            icon={<IconFont type='icon-php' />}
             onClick={() => goTo(PhpReverseShell)}
           >
             PHP Reverse Shell
           </Menu.Item>
           <Menu.Item
             key='9'
-            icon={<FileOutlined />}
+            icon={<IconFont type='icon-lvzhou_yuanchengTelnet' />}
             onClick={() => goTo(TtySpawnShell)}
           >
             TTY Spawn Shell
           </Menu.Item>
-          <SubMenu key='sub2' icon={<TeamOutlined />} title='Team'>
+          <SubMenu
+            key='sub2'
+            icon={<IconFont type='icon-sort_others' />}
+            title='Team'
+          >
             <Menu.Item key='6'>Team 1</Menu.Item>
             <Menu.Item key='8'>Team 2</Menu.Item>
           </SubMenu>
+          <Menu.Item
+            key='9'
+            icon={<IconFont type='icon-about' />}
+            onClick={() => goTo(TtySpawnShell)}
+          >
+            About us
+          </Menu.Item>
         </Menu>
       </Sider>
       <Layout className='site-layout'>
