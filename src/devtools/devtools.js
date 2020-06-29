ebb0f96a9948cbd99194b21732c04975d8782341
Rb-x <riyu.95400@gmail.com>
2020-06-29 21:06:34 +0200

    mozilla src

--- src/devtools/devtools.js
+++ src/devtools/devtools.js
@@ -1,6 +1,5 @@
 browser.devtools.panels.create(
   "HackTools", // title for the panel tab
-  ":get_started16.png", // you can specify here path to an icon
-  "index.html", // html page which is gonna be injecting into the tab's content
-  null // you can pass here a callback function
+  "get_started16.png", // you can specify here path to an icon
+  "index.html" // html page which is gonna be injecting into the tab's content
 );
