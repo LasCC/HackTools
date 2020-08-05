42fd9c8e4418b194f6e93b882f9aa3c402657351
Riadh Bch <47902351+rb-x@users.noreply.github.com>
2020-08-05 18:03:59 +0200

    Update devtools.js
    compatibility mozilla / chrome

--- src/devtools/devtools.js
+++ src/devtools/devtools.js
@@ -1,6 +1,14 @@
-chrome.devtools.panels.create(
-  "HackTools", // title for the panel tab
-  null, // you can specify here path to an icon
-  "index.html", // html page which is gonna be injecting into the tab's content
-  null // you can pass here a callback function
-);
+navigator.userAgent.indexOf("Firefox") != -1 &&
+  browser.devtools.panels.create(
+    "HackTools",
+    "get_started16.png",
+    "index.html"
+  );
+
+navigator.userAgent.indexOf("Chrome") != -1 &&
+  chrome.devtools.panels.create(
+    "HackTools", // title for the panel tab
+    null, //   path to an icon
+    "index.html", // html page which is gonna be injecting into the tab's content
+    null // callback function here
+  );
