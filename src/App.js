016adaa8846be305a5edf9d05ac900b10c0d10f6
LasCC <erazjean@gmail.com>
2020-08-28 02:08:21 +0200

    Fixing some bugs 🥴

--- src/App.js
+++ src/App.js
@@ -4,22 +4,23 @@ import LayoutApp from "./components/LayoutApp.js";
 import "./assets/css/style.css";
 import ReactDOM from "react-dom";
 import { Router } from "react-chrome-extension-router";
+
 import ReverseShell from "./components/ReverseShell.js";
 
 function App() {
-	return (
-		<div>
-			<ReverseShell />
-		</div>
-	);
+  return (
+    <div>
+      <ReverseShell />
+    </div>
+  );
 }
 
 ReactDOM.render(
-	<LayoutApp>
-		<Router>
-			<App />
-		</Router>
-		<BackTop />
-	</LayoutApp>,
-	document.getElementById("app")
+  <LayoutApp>
+    <Router>
+      <App />
+    </Router>
+    <BackTop />
+  </LayoutApp>,
+  document.getElementById("app")
 );
