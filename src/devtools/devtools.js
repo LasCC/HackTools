f0c8e44fc07e27eff08d3bf08ebb635037d15809
Ludovic COULON <43119034+LasCC@users.noreply.github.com>
2021-01-01 19:46:55 +0100

    Update devtools.js

--- src/devtools/devtools.js
+++ src/devtools/devtools.js
@@ -1,14 +1,10 @@
-navigator.userAgent.indexOf("Firefox") != -1 &&
-	browser.devtools.panels.create(
-		"HackTools",
-		"get_started16.png",
-		"index.html"
-	);
+navigator.userAgent.indexOf('Firefox') != -1 &&
+	browser.devtools.panels.create('HackTools', 'get_started16.png', 'index.html');
 
-navigator.userAgent.indexOf("Chrome") != -1 &&
+navigator.userAgent.indexOf('Chrome') != -1 &&
 	chrome.devtools.panels.create(
-		"HackTools", // title for the panel tab
+		'HackTools', // title for the panel tab
 		null, //   path to an icon
-		"index.html", // html page which is gonna be injecting into the tab's content
+		'index.html', // html page which is gonna be injecting into the tab's content
 		null // callback function here
 	);
