5198ef34abbc58c3ea4950de3b20855d10f0e677
Ludovic COULON <coulonludovicc@gmail.com>
2021-05-28 21:41:10 +0200

    Fix RFI Payload

--- src/components/web/LFI.js
+++ src/components/web/LFI.js
@@ -8,11 +8,13 @@ const { Title, Paragraph } = Typography;
 
 export default (props) => {
 	const successInfoReverseShell = () => {
-		message.success('Your LFI payload has been copied');
+		message.success('Your payload has been copied');
 	};
+
 	const successInfoEncodeURL = () => {
-		message.success('Your LFI payload URL encoded has been copied');
+		message.success('Your payload URL encoded has been copied');
 	};
+
 	const directoryTraversal = `foo.php?file=../../../../../../../etc/passwd`;
 	const phpWrapperLfi = `/example1.php?page=expect://ls`;
 	const phpWrapperFilter = `/example1.php?page=php://filter/convert.base64-encode/resource=../../../../../etc/passwd`;
@@ -64,6 +66,7 @@ export default (props) => {
 		{ title: '/windows/system32/license.rtf' },
 		{ title: '/windows/system32/eula.txt' }
 	];
+
 	return (
 		<QueueAnim delay={300} duration={1500}>
 			<Title variant='Title level={3}' style={{ fontWeight: 'bold', margin: 15 }}>
@@ -180,7 +183,7 @@ export default (props) => {
 				<Clipboard component='a' data-clipboard-text={phpRfi}>
 					<Button
 						type='primary'
-						onClick={message.success('Your RFI payload has been copied')}
+						onClick={successInfoReverseShell}
 						style={{ marginBottom: 10, marginTop: 15 }}
 					>
 						<CopyOutlined />
@@ -190,7 +193,7 @@ export default (props) => {
 				<Clipboard component='a' data-clipboard-text={encodeURI(phpRfi)}>
 					<Button
 						type='dashed'
-						onClick={message.success('Your RFI payload URL encoded has been copied')}
+						onClick={successInfoEncodeURL}
 						style={{ marginBottom: 10, marginTop: 15, marginLeft: 15 }}
 					>
 						<LinkOutlined /> URL encoded
