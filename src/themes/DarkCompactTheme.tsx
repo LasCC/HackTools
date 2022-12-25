76402f75353cc6da9e95acd42cd5bbc49c847e23
Ludovic COULON <coulonludovicc@gmail.com>
2022-12-25 21:17:54 +0100

    remove theme component

--- src/themes/DarkCompactTheme.tsx
+++
@@ -1,8 +0,0 @@
-// @ts-ignore
-import styles from './dark-compact.theme.less';
-import { useApplyStyles } from '../theming';
-
-export default function DarkTheme () {
-    useApplyStyles( styles );
-    return null;
-}
--- src/themes/LightCompactTheme.tsx
+++
@@ -1,8 +0,0 @@
-// @ts-ignore
-import styles from './light-compact.theme.less';
-import { useApplyStyles } from '../theming';
-
-export default function LightCompactTheme () {
-    useApplyStyles( styles );
-    return null;
-}
