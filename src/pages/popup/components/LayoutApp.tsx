e996b106814179dbffa63fdca0ce43674d3077cd
Riadh BCH <riadh.bch@outlook.fr>
2023-04-10 01:39:13 +0200

    change relative path for popup

--- src/pages/popup/components/LayoutApp.tsx
+++ src/pages/popup/components/LayoutApp.tsx
@@ -169,7 +169,7 @@ export default function LayoutApp ( props: {
         const height = 800;
 
         chrome.windows.create( {
-            url: chrome.runtime.getURL( 'index.html' ),
+            url: chrome.runtime.getURL( '/src/pages/popup/index.html' ),
             width: width,
             height: height,
             type: 'popup'
