fb6712c9f5018abdf6b47ca2796ef8b30206c8e0
Riadh B <riadh.bch@outlook.fr>
2023-09-10 03:03:29 +0200

    Adding Apk build && signing command

--- src/pages/popup/components/mobile/Android/ADB/ADB.tsx
+++ src/pages/popup/components/mobile/Android/ADB/ADB.tsx
@@ -1,12 +1,10 @@
-import { useEffect, useState } from 'react';
 import { WifiOutlined } from '@ant-design/icons';
-import { Col, Divider, Input, Row, Select, Table, Typography } from 'antd';
+import { Col, Divider, Input, Row, Select, Typography } from 'antd';
 import { FaDoorOpen, FaUsb } from 'react-icons/fa';
 import { TbBrandAndroid } from 'react-icons/tb';
+import adbCommands from '../../../../assets/data/Mobile/ADB.json';
 import useADBStore, { ADB_MODE } from './store/ADBStore';
-import adbCommands from '../../../../assets/data/Mobile/ADB.json'
 const { Title, Paragraph, Text } = Typography;
-
 
 
 const ADB = () => {
@@ -16,31 +14,29 @@ const ADB = () => {
     handleIPChange,
     adb_port,
     handlePortChange } = useADBStore()
-    
-    
-    
-    const handleCommand = (command) => {
-      if (adb_ip && command.command.includes("${device}")) {
-        if (mode === ADB_MODE.REMOTE) {
-          return command.command.replace(/\${device}/g, String("-s " + adb_ip + ":" + (adb_port? adb_port: "5555")));
-        } else if (mode === ADB_MODE.LOCAL) {
-          return command.command.replace(/\${device}/g, String("-s " + adb_ip));
-        }
-      } else {
-        return command.command.replace(/\${device}/g, "");
-      }
-    }
 
-    const ADB_OPTIONS = [
-      { value: 'USB', label: <><FaUsb />{" USB"}</> },
-      { value: 'Remote', label: <><WifiOutlined />{" Remote"}</> },
-    ];
-    const defaultValue = mode === ADB_MODE.LOCAL ? ADB_OPTIONS[0].value : ADB_OPTIONS[1].value;
-    return (
+  const handleCommand = (command) => {
+    if (adb_ip && command.command.includes("${device}")) {
+      if (mode === ADB_MODE.REMOTE) {
+        return command.command.replace(/\${device}/g, String("-s " + adb_ip + ":" + (adb_port ? adb_port : "5555")));
+      } else if (mode === ADB_MODE.LOCAL) {
+        return command.command.replace(/\${device}/g, String("-s " + adb_ip));
+      }
+    } else {
+      return command.command.replace(/\${device}/g, "");
+    }
+  }
+
+  const ADB_OPTIONS = [
+    { value: 'USB', label: <><FaUsb />{" USB"}</> },
+    { value: 'Remote', label: <><WifiOutlined />{" Remote"}</> },
+  ];
+  const defaultValue = mode === ADB_MODE.LOCAL ? ADB_OPTIONS[0].value : ADB_OPTIONS[1].value;
+  return (
     <>
       <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
         <Col span={8}>
-        <Select
+          <Select
             defaultValue={defaultValue}
             style={{ width: "100%" }}
             onChange={handleModeChange}
@@ -81,7 +77,7 @@ const ADB = () => {
           <Paragraph>{command.description}</Paragraph>
           <Paragraph>
             <pre><Text copyable>
-            {handleCommand(command)}
+              {handleCommand(command)}
             </Text></pre>
           </Paragraph>
         </div >
--- src/pages/popup/components/mobile/Android/ADB/APKPatching.tsx
+++ src/pages/popup/components/mobile/Android/ADB/APKPatching.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { Typography, Divider } from 'antd';
+import apkPatchingCommands from '../../../../assets/data/Mobile/APK_PATCHING.json';
+
+const { Title, Paragraph, Text } = Typography;
 
 const APKPatching = () => {
+  const handleCommand = (command) => {
+    return command.command;
+  }
+
   return (
-    <div>APKPatching</div>
+    <>
+      {apkPatchingCommands.map((command, index) => (
+        <div key={index}>
+          <Title level={4}>{command.name}</Title>
+          <Paragraph>{command.description}</Paragraph>
+          <Paragraph>
+            <pre><Text copyable>
+              {handleCommand(command)}
+            </Text></pre>
+          </Paragraph>
+        </div>
+      ))}
+      <Divider />
+    </>
   )
 }
 
-export default APKPatching
+export default APKPatching;
