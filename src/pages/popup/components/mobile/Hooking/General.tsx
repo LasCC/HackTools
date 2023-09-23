ab9869d925e2eebc651ed5117f66ae90257fcbf2
Riadh B <riadh.bch@outlook.fr>
2023-09-23 19:28:46 +0200

    react-cmdk demo

--- src/pages/popup/components/mobile/Hooking/General.tsx
+++ src/pages/popup/components/mobile/Hooking/General.tsx
@@ -1,9 +1,102 @@
-import React from 'react'
+import "react-cmdk/dist/cmdk.css";
+import CommandPalette, { filterItems, getItemIndex } from "react-cmdk";
+import { useState } from "react";
 
-const General = () => {
-    return (
-        <div>General</div>
-    )
-}
+const Example = () => {
+  const [page, setPage] = useState<"root" | "projects">("root");
+  const [open, setOpen] = useState<boolean>(true);
+  const [search, setSearch] = useState("");
 
-export default General
+  const filteredItems = filterItems(
+    [
+      {
+        heading: "Home",
+        id: "home",
+        items: [
+          {
+            id: "home",
+            children: "Home",
+            icon: "HomeIcon",
+            href: "#",
+          },
+          {
+            id: "settings",
+            children: "Settings",
+            icon: "CogIcon",
+            href: "#",
+          },
+          {
+            id: "projects",
+            children: "Projects",
+            icon: "RectangleStackIcon",
+            closeOnSelect: false,
+            onClick: () => {
+              setPage("projects");
+            },
+          },
+        ],
+      },
+      {
+        heading: "Other",
+        id: "advanced",
+        items: [
+          {
+            id: "developer-settings",
+            children: "Developer settings",
+            icon: "CodeBracketIcon",
+            href: "#",
+          },
+          {
+            id: "privacy-policy",
+            children: "Privacy policy",
+            icon: "LifebuoyIcon",
+            href: "#",
+          },
+          {
+            id: "log-out",
+            children: "Log out",
+            icon: "ArrowRightOnRectangleIcon",
+            onClick: () => {
+              alert("Logging out...");
+            },
+          },
+        ],
+      },
+    ],
+    search
+  );
+
+  return (
+    <CommandPalette
+      onChangeSearch={setSearch}
+      onChangeOpen={setOpen}
+      search={search}
+      isOpen={open}
+      page={page}
+    >
+      <CommandPalette.Page id="root">
+        {filteredItems.length ? (
+          filteredItems.map((list) => (
+            <CommandPalette.List key={list.id} heading={list.heading}>
+              {list.items.map(({ id, ...rest }) => (
+                <CommandPalette.ListItem
+                  key={id}
+                  index={getItemIndex(filteredItems, id)}
+                  {...rest}
+                />
+              ))}
+            </CommandPalette.List>
+          ))
+        ) : (
+          <CommandPalette.FreeSearchAction />
+        )}
+      </CommandPalette.Page>
+
+      <CommandPalette.Page id="projects">
+        {/* Projects page */}
+      </CommandPalette.Page>
+    </CommandPalette>
+  );
+};
+
+export default Example;
