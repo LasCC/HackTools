509847167d428c416f5eb6f8b889d91682fd0511
LasCC <coulonludovicc@gmail.com>
2023-09-23 17:30:25 +0200

    update vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,11 @@ import customDynamicImport from "./utils/plugins/custom-dynamic-import";
 import addHmr from "./utils/plugins/add-hmr";
 import manifest from "./manifest";
 import { viteStaticCopy } from 'vite-plugin-static-copy'
-const root = resolve(__dirname, "src");
-const pagesDir = resolve(root, "pages");
-const assetsDir = resolve(root, "assets");
-const outDir = resolve(__dirname, "dist");
-const publicDir = resolve(__dirname, "public");
+const root = resolve( __dirname, "src" );
+const pagesDir = resolve( root, "pages" );
+const assetsDir = resolve( root, "assets" );
+const outDir = resolve( __dirname, "dist" );
+const publicDir = resolve( __dirname, "public" );
 
 
 const isDev = process.env.__DEV__ === "true";
@@ -20,103 +20,103 @@ const isProduction = !isDev;
 // ENABLE HMR IN BACKGROUND SCRIPT
 const enableHmrInBackgroundScript = true;
 
-export default defineConfig({
-  resolve: {
-    alias: {
-      "@src": root,
-      "@assets": assetsDir,
-      "@pages": pagesDir,
-    },
-  },
-  plugins: [
-    react(),
-    makeManifest(manifest, {
-      isDev,
-      contentScriptCssKey: regenerateCacheInvalidationKey(),
-    }),
-    customDynamicImport(),
-    addHmr({ background: enableHmrInBackgroundScript, view: true }),
-    viteStaticCopy({
-      targets: [
-        {
-          src: resolve(pagesDir, "popup", "iconfont.js"),
-          dest: resolve(outDir, "src/pages/popup/"),
-        },
-      ],
-    }),
-  ],
-  optimizeDeps: {
-    esbuildOptions: {
-        // Node.js global to browser globalThis
-        define: {
-            global: 'globalThis',
+export default defineConfig( {
+    resolve: {
+        alias: {
+            "@src": root,
+            "@assets": assetsDir,
+            "@pages": pagesDir,
         },
     },
-},
-  publicDir,
-  build: {
-    outDir,
-    /** Can slowDown build speed. */
-    // sourcemap: isDev,
-    minify: isProduction,
-    reportCompressedSize: isProduction,
-    rollupOptions: {
-      input: {
-        devtools: resolve(pagesDir, "devtools", "index.html"),
-        // panel: resolve(pagesDir, "panel", "index.html"),
-        content: resolve(pagesDir, "content", "index.ts"),
-        background: resolve(pagesDir, "background", "index.ts"),
-        // contentStyle: resolve(pagesDir, "content", "style.scss"),
-        popup: resolve(pagesDir, "popup", "index.html"),
-        // newtab: resolve(pagesDir, "newtab", "index.html"),
-        options: resolve(pagesDir, "options", "index.html")
-      },
-      watch: {
-        include: ["src/**", "vite.config.ts"],
-        exclude: ["node_modules/**", "src/**/*.spec.ts"],
-      },
-      output: {
-        entryFileNames: "src/pages/[name]/index.js",
-  
-        manualChunks: (id) => {
-          const modules_to_bundle = [
-            "node-sql-parser",
-            "antd",
-          ]
-
-          if (id.includes('node_modules') && !modules_to_bundle.some(module => id.includes(module))) {
-            return 'vendor'
-          }
+    plugins: [
+        react(),
+        makeManifest( manifest, {
+            isDev,
+            contentScriptCssKey: regenerateCacheInvalidationKey(),
+        } ),
+        customDynamicImport(),
+        addHmr( { background: enableHmrInBackgroundScript, view: true } ),
+        viteStaticCopy( {
+            targets: [
+                {
+                    src: resolve( pagesDir, "popup", "iconfont.js" ),
+                    dest: resolve( outDir, "src/pages/popup/" ),
+                },
+            ],
+        } ),
+    ],
+    optimizeDeps: {
+        esbuildOptions: {
+            // Node.js global to browser globalThis
+            define: {
+                global: 'globalThis',
+            },
         },
-
-        chunkFileNames: isDev
-          ? "assets/js/[name].js"
-          : "assets/js/[name].[hash].js",
-        assetFileNames: (assetInfo) => {
-          const { dir, name: _name } = path.parse(assetInfo.name);
-          const assetFolder = dir.split("/").at(-1);
-          const name = assetFolder + firstUpperCase(_name);
-          if (name === "contentStyle") {
-            return `assets/css/contentStyle${cacheInvalidationKey}.chunk.css`;
-          }
-          return `assets/[ext]/${name}.chunk.[ext]`;
-        },
-      },
     },
-  },
-});
+    publicDir,
+    build: {
+        outDir,
+        /** Can slowDown build speed. */
+        // sourcemap: isDev,
+        minify: isProduction,
+        reportCompressedSize: isProduction,
+        rollupOptions: {
+            input: {
+                devtools: resolve( pagesDir, "devtools", "index.html" ),
+                popup: resolve( pagesDir, "popup", "index.html" ),
+                background: resolve( pagesDir, "background", "index.ts" ),
+                // panel: resolve(pagesDir, "panel", "index.html"),
+                // content: resolve( pagesDir, "content", "index.ts" ),
+                // contentStyle: resolve(pagesDir, "content", "style.scss"),
+                // newtab: resolve(pagesDir, "newtab", "index.html"),
+                // options: resolve( pagesDir, "options", "index.html" )
+            },
+            watch: {
+                include: [ "src/**", "vite.config.ts" ],
+                exclude: [ "node_modules/**", "src/**/*.spec.ts" ],
+            },
+            output: {
+                entryFileNames: "src/pages/[name]/index.js",
 
-function firstUpperCase(str: string) {
-  const firstAlphabet = new RegExp(/( |^)[a-z]/, "g");
-  return str.toLowerCase().replace(firstAlphabet, (L) => L.toUpperCase());
+                manualChunks: ( id ) => {
+                    const modules_to_bundle = [
+                        "node-sql-parser",
+                        "antd",
+                    ]
+
+                    if ( id.includes( 'node_modules' ) && !modules_to_bundle.some( module => id.includes( module ) ) ) {
+                        return 'vendor'
+                    }
+                },
+
+                chunkFileNames: isDev
+                    ? "assets/js/[name].js"
+                    : "assets/js/[name].[hash].js",
+                assetFileNames: ( assetInfo ) => {
+                    const { dir, name: _name } = path.parse( assetInfo.name );
+                    const assetFolder = dir.split( "/" ).at( -1 );
+                    const name = assetFolder + firstUpperCase( _name );
+                    if ( name === "contentStyle" ) {
+                        return `assets/css/contentStyle${ cacheInvalidationKey }.chunk.css`;
+                    }
+                    return `assets/[ext]/${ name }.chunk.[ext]`;
+                },
+            },
+        },
+    },
+} );
+
+function firstUpperCase ( str: string ) {
+    const firstAlphabet = new RegExp( /( |^)[a-z]/, "g" );
+    return str.toLowerCase().replace( firstAlphabet, ( L ) => L.toUpperCase() );
 }
 
 let cacheInvalidationKey: string = generateKey();
-function regenerateCacheInvalidationKey() {
-  cacheInvalidationKey = generateKey();
-  return cacheInvalidationKey;
+function regenerateCacheInvalidationKey () {
+    cacheInvalidationKey = generateKey();
+    return cacheInvalidationKey;
 }
 
-function generateKey(): string {
-  return `${Date.now()}`;
+function generateKey (): string {
+    return `${ Date.now() }`;
 }
