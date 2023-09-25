066d6e5edacf937f29a6880d4f7131b509ae9478
Riadh B <riadh.bch@outlook.fr>
2023-09-25 23:47:02 +0200

    pre-store migration

--- src/pages/popup/components/mobile/Android/ADB/IntentInjection.tsx
+++ src/pages/popup/components/mobile/Android/ADB/IntentInjection.tsx
@@ -1,15 +1,36 @@
 import React, { useState } from 'react';
-import { Input, Button, Row, Col, Form, Space, Typography } from 'antd';
-import { PlusOutlined } from '@ant-design/icons';
+import { Input, Button, Row, Col, Form, Space, Typography, Select } from 'antd';
+import { DeleteOutlined, MinusOutlined, PlusOutlined } from '@ant-design/icons';
 import { vs2015 } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 
-const IntentInjection = () => {
-    const [className, setClassName] = useState('');
-    const [extras, setExtras] = useState([{ key: '', value: '' }]);
-    const [adbCommand, setAdbCommand] = useState('');
-    const [javaPOC, setJavaPOC] = useState('');
+const { Option } = Select;
 
+interface Extra {
+    key: string;
+    value: string;
+    type: 'string' | 'boolean' | 'integer'; // // TODO: Add more cases here for other intent types
+}
+
+const IntentInjection = () => {
+    const [className, setClassName] = useState<string>('');
+    const [extras, setExtras] = useState<Extra[]>([{ key: '', value: '', type: 'string' }]);
+    const [adbCommand, setAdbCommand] = useState<string>('');
+    const [javaPOC, setJavaPOC] = useState<string>('');
+
+    const addExtra = () => {
+        setExtras([...extras, { key: '', value: '', type: 'string' }]);
+    };
+
+    const clearAll = () => {
+        setExtras([]);
+    };
+
+    const deleteExtra = (index: number) => {
+        const newExtras = [...extras];
+        newExtras.splice(index, 1);
+        setExtras(newExtras);
+    };
 
 
     const generatePOC = () => {
@@ -18,7 +39,7 @@ const IntentInjection = () => {
         let activityName = '';
 
         if (trimmedClassName.startsWith('<')) {
-            const match = trimmedClassName.match(/<activity[^>]*android:name="([^"]+)"/);
+            const match = trimmedClassName.match(/<activity[^>]*android:name="([^"]+)"/i);
             if (match) {
                 const fullClassName = match[1];
                 const splitName = fullClassName.split('.');
@@ -31,23 +52,45 @@ const IntentInjection = () => {
             activityName = splitName[splitName.length - 1];
         }
 
-        const extrasString = extras.map(extra => `--es "${extra.key}" "${extra.value}"`).join(' ');
+
+        const extrasString = extras.map(extra => {
+            switch (extra.type) {
+                case 'string':
+                    return `--es "${extra.key}" "${extra.value}"`;
+                case 'boolean':
+                    return `--ez "${extra.key}" ${extra.value}`;
+                case 'integer':
+                    return `--ei "${extra.key}" ${extra.value}`;
+                // TODO: Add more cases here for other types
+                default:
+                    return '';
+            }
+        }).join(' ');
         const command = `adb shell am start -n ${packageName}/.${activityName} ${extrasString}`;
         setAdbCommand(command);
 
-        const extrasJavaString = extras.map(extra => `intent.putExtra("${extra.key}", "${extra.value}");`).join('\n');
+        const extrasJavaString = extras.map(extra => {
+            switch (extra.type) {
+                case 'string':
+                    return `intent.putExtra("${extra.key}", "${extra.value}");`;
+                case 'boolean':
+                    return `intent.putExtra("${extra.key}", ${extra.value});`;
+                case 'integer':
+                    return `intent.putExtra("${extra.key}", ${extra.value});`;
+                // TODO: Add more cases here for other types
+                default:
+                    return '';
+            }
+        }).join('\n');
         const javaPOCCode = `
-    Intent intent = new Intent();
-    intent.setClassName("${packageName}", "${packageName}.${activityName}");
-    ${extrasJavaString}
-    startActivity(intent);
-    `.split('\n').map(line => line.trim()).join('\n');
+// Java POC
+Intent intent = new Intent();
+intent.setClassName("${packageName}", "${packageName}.${activityName}");
+${extrasJavaString}
+startActivity(intent);
+`.split('\n').map(line => line.trim()).join('\n');
 
         setJavaPOC(javaPOCCode);
-    };
-
-    const addExtra = () => {
-        setExtras([...extras, { key: '', value: '' }]);
     };
 
     return (
@@ -55,10 +98,23 @@ const IntentInjection = () => {
             <Col span={24}>
                 <Form onFinish={generatePOC}>
                     <Form.Item label="Class">
-                        <Input placeholder="Enter class" value={className} onChange={e => setClassName(e.target.value)} />
+                        <Input placeholder={`Provide com.app.RenderWebViewActivity or the whole xml tag : <activity android:name='com.app.RenderWebViewActivity' .../>`}
+                            value={className} onChange={e => setClassName(e.target.value)} />
                     </Form.Item>
                     {extras.map((extra, index) => (
                         <Space key={index} style={{ display: 'flex', marginBottom: 8 }} align="baseline">
+                            <Form.Item label={`Extra ${index + 1} Type`}>
+                                <Select value={extra.type} onChange={value => {
+                                    const newExtras = [...extras];
+                                    newExtras[index].type = value as 'string' | 'boolean' | 'integer'; 
+                                    setExtras(newExtras);
+                                }}>
+                                    <Option value="string">String</Option>
+                                    <Option value="boolean">Boolean</Option>
+                                    <Option value="integer">Integer</Option>
+                                    {/* Add more options here for other types */}
+                                </Select>
+                            </Form.Item>
                             <Form.Item label={`Extra ${index + 1} Key`}>
                                 <Input placeholder="Enter key" value={extra.key} onChange={e => {
                                     const newExtras = [...extras];
@@ -73,6 +129,13 @@ const IntentInjection = () => {
                                     setExtras(newExtras);
                                 }} />
                             </Form.Item>
+                            <Form.Item>
+                                <Button type="primary"
+                                    icon={<MinusOutlined />}
+                                    danger onClick={() => deleteExtra(index)}>
+                                    Delete
+                                </Button>
+                            </Form.Item>
                         </Space>
                     ))}
                     <Form.Item>
@@ -81,7 +144,13 @@ const IntentInjection = () => {
                         </Button>
                     </Form.Item>
                     <Form.Item>
-                        <Button type="primary" htmlType="submit">Generate ADB POC</Button>
+                        <Button type="primary"
+                            icon={<DeleteOutlined />} danger onClick={clearAll}>
+                            Clear All
+                        </Button>
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit">Generate POCs</Button>
                     </Form.Item>
                 </Form>
                 <Typography.Paragraph copyable code>
