cfc7abca07dbafc609dbc34df585ef506eadb3a3
Riadh B <riadh.bch@outlook.fr>
2023-09-26 01:01:34 +0200

    switching to suffix

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 CONTAINER_NAME="hacktools-build"
 
-sudo docker build -t $CONTAINER_NAME -f builder.Dockerfile .
+sudo docker build -t $CONTAINER_NAME -f Dockerfile.build .
 sudo docker run -it --rm -v $(pwd)/dist:/usr/src/app/dist $CONTAINER_NAME
 sudo chown -R $(id -u):$(id -g) $(pwd)/dist
