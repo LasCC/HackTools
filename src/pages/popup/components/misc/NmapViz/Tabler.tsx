167ae6517860bc0062b0e628313044f4c1fb6d69
Riadh B <riadh.bch@outlook.fr>
2023-10-29 00:32:45 +0200

    prewrap results

--- src/pages/popup/components/misc/NmapViz/Tabler.tsx
+++ src/pages/popup/components/misc/NmapViz/Tabler.tsx
@@ -1,9 +1,9 @@
 import { useEffect } from 'react';
-import { Table, Input, Row, Col, Collapse } from 'antd';
+import { Table, Input, Row, Col, Collapse, Typography } from 'antd';
 import useStore from './store';
 
 const Tabler = () => {
-    const { data, queryData, tableData, searchQuery , setSearchQuery } = useStore();
+    const { data, queryData, tableData, searchQuery, setSearchQuery } = useStore();
 
     const columns = [
         {
@@ -47,13 +47,14 @@ const Tabler = () => {
             </Col>
             <Col span={24}>
                 <Collapse
-                    defaultActiveKey={['1']}
                     ghost
                     items={[
                         {
                             key: '1',
                             label: `Query result ~ (${data[0] && Object.keys(data[0]).length > 0 ? data.length : 0}) services`,
-                            children: <pre>{JSON.stringify(data, null, 2)}</pre>
+                            children: <pre style={{ whiteSpace: 'pre-wrap', wordWrap: 'break-word' }}>
+                                {JSON.stringify(data, null, 2)}
+                            </pre>
                         }
                     ]}
                 />
