4dfdf2b3841dc563255666e991d70fe9ceab2a85
LasCC <coulonludovicc@gmail.com>
2023-11-11 17:07:14 +0100

    Fix chrome url popup

--- entrypoints/popup/components/CommandNavigation.tsx
+++ entrypoints/popup/components/CommandNavigation.tsx
@@ -148,7 +148,7 @@ const CommandNavigation = () => {
               const width = 1100;
               const height = 800;
               chrome.windows.create({
-                url: chrome.runtime.getURL("/src/pages/popup/index.html"),
+                url: chrome.runtime.getURL("/popup.html"),
                 width: width,
                 height: height,
                 type: "popup",
