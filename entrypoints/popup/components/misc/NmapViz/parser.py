3af1422fc2ef3d4ec7c5519bc85ecd26213d62e7
Riadh B <riadh.bch@outlook.fr>
2023-11-16 01:39:40 +0100

    Cves,smb_signing,highest CVSS parsing

--- entrypoints/popup/components/misc/NmapViz/parser.py
+++ entrypoints/popup/components/misc/NmapViz/parser.py
@@ -7,11 +7,68 @@ nmap_report = NmapParser.parse_fromfile(sys.argv[1])
 
 services = []
 
+
+def extract_highest_cvss_from_vulners_scripts(service_data, scripts_results):
+    highest_cvss = 0
+    for script_result in scripts_results:
+        if script_result.get('id') == 'vulners':
+            for cpe, cpe_data in script_result.get('elements', {}).items():
+                print(cpe_data)
+                for vulnerability in cpe_data.get(None, []):
+                    print(vulnerability)
+                    cvss = float(vulnerability.get('cvss', '0'))
+                    if cvss > highest_cvss:
+                        highest_cvss = cvss
+    service_data['highest_cvss'] = highest_cvss
+    return service_data
+
+
+def simplify_elements(scripts_results):
+    for script_result in scripts_results:
+        if script_result.get('id') == 'vulners':
+            for cpe, cpe_data in script_result.get('elements', {}).items():
+                script_result['elements'][cpe] = cpe_data.get(None, [])
+    return scripts_results
+
+
+
+def extract_is_exploitable(service_data, scripts_results):
+    is_exploitable = False
+    for script_result in scripts_results:
+        if script_result.get('id') == 'vulners':
+            for cpe, cpe_data in script_result.get('elements', {}).items():
+                for vulnerability in cpe_data.get(None, []):
+                    if vulnerability.get('is_exploit') == 'true':
+                        is_exploitable = True
+                        break
+    service_data['is_exploitable'] = is_exploitable
+    return service_data
+
+
+def extract_smb_signing_status(service_data, scripts_results):
+    smb_signing_status = "unknown"
+    for script_result in scripts_results:
+        print(script_result)
+        if script_result.get('id') == 'smb-security-mode':
+            smb_signing_status = script_result.get('output', '').split('\n')[3].strip()
+            break
+    service_data['smb_signing_status'] = smb_signing_status
+    return service_data
+
+
+def extract_smb_signing_status(host):
+    smb_signing_status = "unknown"
+    for script in host.scripts_results:
+        if script.get('id') == 'smb-security-mode':
+            smb_signing_status = script.get('elements', {}).get('message_signing', 'unknown')
+            break
+    return smb_signing_status
+
 # Iterate over each host in the report
 for host in nmap_report.hosts:
-    # Iterate over each service in the host
+    smb_signing_status = extract_smb_signing_status(host)
+    # exit()
     for service in host.services:
-        # Create a dictionary to hold the data for this service
         service_data = {
             "id": f"{host.address}-{service.port}",
             "address": host.address,
@@ -23,14 +80,17 @@ for host in nmap_report.hosts:
             "protocol": service.protocol,
             "service": service.service,
             "banner": service.banner,
-            "scripts_results": service.scripts_results,
-            "metadata": ""
-        }
+            "scripts_results": (service.scripts_results),
+            "smb_signing_status": smb_signing_status if service.port == 445 else "not applicable",
 
-        # Add the service data to the list of services
+            "tag" : "NONE" # EXPLOITED
+        }
+        service_data = extract_highest_cvss_from_vulners_scripts(service_data, service.scripts_results)
+        service_data = extract_is_exploitable(service_data, service.scripts_results)
+        
+
         services.append(service_data)
     
 with open(sys.argv[1] + '.json', 'w') as f:
     json.dump(services, f, indent=2)
     print(json.dumps(services))
-    
